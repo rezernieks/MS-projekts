79288919ec90ff9d3dd9c4f66eab3cffcf8e32e8
rezernieks <59909616+rezernieks@users.noreply.github.com>
2022-12-07 16:37:21 +0200

    :/

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
 
-const Minio = require('minio')
-const minioClient = new Minio.Client({
-    endPoint: '127.0.0.1',
-    port: 9000,
-    useSSL: false,
-    accessKey: 'minio-root',
-    secretKey: 'M@k0nsk@it1os@na'});
+//const Minio = require('minio')
+//const minioClient = new Minio.Client({
+//    endPoint: '127.0.0.1',
+//    port: 9000,
+//    useSSL: false,
+//    accessKey: 'minio-root',
+//    secretKey: 'M@k0nsk@it1os@na'});
+
+console.log("initiated");
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -17,22 +19,12 @@ app.post('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain')
     res.write('your name:\n')
     console.log(req.body);
-    minioClient.fGetObject('cars', 'Gabriel.webp', '/tmp/photo.jpg', function(err) {
-        if (err) {
-            return console.log(err)
-        }
-        console.log('success')
-    })
+    //minioClient.fGetObject('cars', 'Gabriel.webp', '/tmp/photo.jpg', function(err) {
+    //    if (err) {
+    //        return console.log(err)
+    //    }
+    //    console.log('success')
+    //})
     res.end(JSON.stringify(req.body.name))
 })
 
-app.get('/cars', function(res) {
-    res.setHeader('Content-Type', 'text/plain')
-    minioClient.fGetObject('cars', 'Gabriel.webp', '/tmp/photo.jpg', function(err) {
-        if (err) {
-            return console.log(err)
-        }
-        console.log('success')
-    })
-    res.end("success")
-})
