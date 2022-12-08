f3b6f5ab981fd37c90fc5c9b637b16d00c1819ab
Eduards Evele <56454719+evele-eduard@users.noreply.github.com>
2022-12-08 05:20:11 +0200

    Send pictures with intervals

--- DataSender/server.js
+++ DataSender/server.js
@@ -16,18 +16,20 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 
-/* Test image receiving
+
 app.post('/', function requestHandler(req, res) {
   const base64Data = req.body.img.replace(/^data:image\/png;base64,/, "");
   
   
-
-  fs.writeFile("out.png", base64Data, 'base64', function(err) {
+  
+  
+  res.end();
+  
+  fs.writeFile(__dirname + `\\test\\out${req.body.num}.png`, base64Data, 'base64', function(err) {
   console.log(err);
 });
- res.end();
-  });
-  */
+   
+});
 app.listen(port, () => {
     //console.log(`Example app listening on port ${port}`);
     
@@ -35,28 +37,36 @@ app.listen(port, () => {
 function base64_encode(file) {
     return "data:image/png;base64,"+fs.readFileSync(file, 'base64');
 }
-const img_n = getRndInteger(0, 432);
-const img = base64_encode(__dirname + `\\dataset\\images\\Cars${img_n}.png`);
+//const img_n = getRndInteger(0, 432);
+//const img = base64_encode(__dirname + `\\dataset\\images\\Cars${img_n}.png`);
 //console.log(img);
 
-const data = {
+/*const data = {
     img
-  };
+  };*/
+
 const limits = {
   maxBodyLength: Infinity
 };
   
-  
+  let intervalID = setInterval(() => {
+    const img_n = getRndInteger(0, 432);
+    const img = base64_encode(__dirname + `\\dataset\\images\\Cars${img_n}.png`);
+    
+    const data = {
+      img,
+      num : img_n
+    };
   axios
     .post('http://localhost:3000/', data)
     .then(res => {
-      //console.log(`Status: ${res.status}`)
+      console.log(`Status: ${res.status}`)
       //console.log('Body: ', res.data)
-      console.log(n++);
+      
     })
     .catch(err => {
       console.error(err)
-    });
+    })}, 5000);
 
     function getRndInteger(min, max) {
       return Math.floor(Math.random() * (max - min) ) + min;
