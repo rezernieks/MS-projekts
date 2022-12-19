03ec4199e41abbff11b87ebc8367b45e36bbe069
Captain1101 <danielsrudzits@gmail.com>
2022-12-19 21:57:38 +0200

    Merge pull request #2 from rezernieks/kirill/number-recognition-worker
    Kirill/number recognition worker

--- ReddisApi/index.js
+++ ReddisApi/index.js
@@ -1,29 +1,44 @@
 const redis = require('redis');
 const express = require('express');
-const client = redis.createClient();
+//const client = redis.createClient();
 const app = express();
-
+const bodyParser = require('body-parser');
 const PORT = 6379;
-const HOST = '127.0.0.1';
-client.on("error", function(error) {
+
+const getClient = async () => {
+    const client = redis.createClient()
+    client.on("error", function(error) {
+        console.error(error);
+    })
+    client.on('connect', () => {
+        console.log('redis connected', new Date().toISOString());
+    });
+    await client.connect()
+    return client
+}
+
+/*client.on("error", function(error) {
     console.error(error);
 })
-
+client.connect();*/
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.get('/redis/get/:key', function (req, res) {
     client.get(req.params.key, function(err, reply) {
         res.status(200).send({
-            message: "Value from redis:" + reply
+            message: reply
         });
     });
-    app.delete('/redis/get/:key');
+    client.del(req.params.key);
 })
 
-app.post('/redis/set', function (req, res) {
-    client.set(req.body.key, req.body.value, redis.print);
+app.post('/redis/set', async function (req, res) {
+    const client = await getClient()
+    await client.set(req.body.key, req.body.value)
     res.end("OK");
 })
 
-app.listen(PORT, HOST,() => {
+app.listen(PORT, () => {
     console.log("App listening on port 6379");
 })
 
