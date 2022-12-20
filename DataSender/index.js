067d4bf6331553917c5cb2f2835a70ebd68a852e
Eduards Evele <56454719+evele-eduard@users.noreply.github.com>
2022-12-20 21:33:20 +0200

    removed express

--- DataSender/index.js
+++ DataSender/index.js
@@ -1,20 +1,9 @@
-const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const myParser = require('body-parser');
 
-const app = express();
-
 const MAX_IMG = 432;
-const port = 3000;
-app.use(myParser.json({limit: '200mb'}));
-app.use(myParser.urlencoded({limit: '200mb', extended: true}));
-app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);  
-});
-  
 let intervalID = setInterval(() => {
     const img_n = Math.floor(Math.random() * MAX_IMG);
     const img = "data:image/png;base64," + fs.readFileSync(__dirname + `\\dataset\\images\\Cars${img_n}.png`, 'base64');
