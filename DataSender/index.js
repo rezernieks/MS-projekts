1687ba72e3adbb3eae2a19e861f02355bfe1de3e
Eduards Evele <56454719+evele-eduard@users.noreply.github.com>
2022-12-20 22:10:07 +0200

    indents

--- DataSender/index.js
+++ DataSender/index.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 const fs = require('fs');
-const myParser = require('body-parser');
 
 const MAX_IMG = 432;
 
 let intervalID = setInterval(() => {
-    const img_n = Math.floor(Math.random() * MAX_IMG);
-    const img = "data:image/png;base64," + fs.readFileSync(__dirname + `\\dataset\\images\\Cars${img_n}.png`, 'base64');
-    const data = {img};
+  const img_n = Math.floor(Math.random() * MAX_IMG);
+  const img = "data:image/png;base64," + fs.readFileSync(__dirname + `\\dataset\\images\\Cars${img_n}.png`, 'base64');
+  const data = {img};
   axios
     .post('http://localhost:9000/', data)
     .then(res => {
@@ -15,4 +14,4 @@ let intervalID = setInterval(() => {
     })
     .catch(err => {
       console.error(err)
-    })}, 10000);
+})}, 10000);
