16bbebf63a05c05448099ea3f86a89858772ba97
Eduards Evele <56454719+evele-eduard@users.noreply.github.com>
2022-12-20 22:26:44 +0200

    minor changes

--- DataSender/index.js
+++ DataSender/index.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 const myParser = require('body-parser');
 
 const MAX_IMG = 432;
+const port = 9000;
 
 let intervalID = setInterval(() => {
     const img_n = Math.floor(Math.random() * MAX_IMG);
     const img = "data:image/png;base64," + fs.readFileSync(__dirname + `\\dataset\\images\\Cars${img_n}.png`, 'base64');
     const data = {img};
   axios
-    .post('http://localhost:9000/', data)
+    .post(`http://localhost:${port}/`, data)
     .then(res => {
       console.log(`Status: ${res.status}`)
     })
