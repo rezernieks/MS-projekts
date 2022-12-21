2633defc2923662c610e27c7edbd932b9bcc3b89
Eduards Evele <56454719+evele-eduard@users.noreply.github.com>
2022-12-21 14:32:58 +0200

    updated request url

--- DataSender/index.js
+++ DataSender/index.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const MAX_IMG = 432;
+const MAX_IMG = 52;
 const port = 3000;
 
 let intervalID = setInterval(() => {
   let img_n = Math.floor(Math.random() * MAX_IMG);
-  let img = "data:image/png;base64," + fs.readFileSync(__dirname + `\\dataset\\images\\Cars${img_n}.png`, 'base64');
+  let img = "data:image/jpg;base64," + fs.readFileSync(__dirname + `\\dataset\\eu-license-plates\\car_${img_n}.jpg`, 'base64');
   let data = {img};
   axios 
     .post(`http://127.0.0.1:${port}/post_incoming`, data)
@@ -16,11 +16,11 @@ let intervalID = setInterval(() => {
     .catch(err => {
       console.error(err)
     })
-  img_n = img_n + 1 ? img_n != MAX_IMG : img_n - 1;
-  img = "data:image/png;base64," + fs.readFileSync(__dirname + `\\dataset\\images\\Cars${img_n}.png`, 'base64');
+  img_n = img_n != MAX_IMG ? img_n + 1 : img_n - 1;
+  img = "data:image/png;base64," + fs.readFileSync(__dirname + `\\dataset\\eu-license-plates\\car_${img_n}.jpg`, 'base64');
   data = {img}
   axios 
-    .post(`http://127.0.0.1:${port}/post_incoming`, data)
+    .post(`http://io_handler_api:${port}/post_incoming`, data)
     .then(res => {
       console.log(`Status: ${res.status}`)
     })
