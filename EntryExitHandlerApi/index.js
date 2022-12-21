9e2f9b547dc517af96a59f9c3c70273dc0c6d224
Kirill <kirill.dobrynin78@gmail.com>
2022-12-21 10:40:58 +0200

    incoming/outcoming handler fixes

--- EntryExitHandlerApi/index.js
+++ EntryExitHandlerApi/index.js
@@ -1,110 +1,112 @@
-const express = require('express')
+const fs = require("fs");
+const express = require('express');
 const fileUpload = require('express-fileupload');
-const app = express()
 const bodyParser = require('body-parser');
 const amqp = require('amqplib/callback_api');
+const minio = require('minio');
+
+const app = express();
+
+const minioClient = new minio.Client({
+    endPoint: 'minio',
+    port: 9000,
+    useSSL: false,
+    accessKey: 'minio-root',
+    secretKey: 'M@k0nsk@it1os@na'
+});
 
 const rabbitmq_username = 'rabbit-bunny';
 const rabbitmq_password = 'M@k0nsk@it1os@na';
 const rabbitmq_url = 'rabbitmq:5672';
 
-const Minio = require('minio')
-const fs = require("fs");
-const minioClient = new Minio.Client({
-    endPoint: 'minio',
-    port: 9000,
-    useSSL: false,
-    accessKey: 'minio-root',
-    secretKey: 'M@k0nsk@it1os@na'});
-
-
-
 function rabbit_sender(data){
     amqp.connect(`amqp://${rabbitmq_username}:${rabbitmq_password}@${rabbitmq_url}`, function(error0, connection) {
-        if (error0) { throw error0; }
+        if (error0) throw error0;
+
         connection.createChannel(function(error1, channel) {
-            if (error1) {
-                throw error1;
-            }
+            if (error1) throw error1;
+
             let queue = 'car-queue';
 
             let msg = JSON.stringify(data);
-            channel.assertQueue(queue, {
-                durable: true
-            });
-            channel.sendToQueue(queue, Buffer.from
-            (msg)); console.log(" [x] Sent %s", msg);
+
+            channel.assertQueue(queue, { durable: true });
+            channel.sendToQueue(queue, Buffer.from(msg));
+
+            console.log(`Sent message ${msg} to RabbitMQ...`);
         });
     });
 }
 
 async function minio_put(filename, path){
     minioClient.fPutObject('cars', filename, path, function (err) {
-        if (err) return console.log(err)
-        console.log('File uploaded successfully.')
+        if (err) return console.log(err);
+        console.log(`[${filename}] uploaded to minio successfully.`);
         fs.unlink(path, (err) => {
-            if (err) {
-                throw err;
-            }
-            console.log("Delete File successfully.");
+            if (err) throw err;
+            console.log(`[${filename}] deleted from local storage successfully.`);
         })
     });
 }
 
-async function upload_photo(image, filename, path) {
-    await image.mv(path, async function (err) {
-        if (err)
-            console.log(err);
-        await minio_put(filename, path, function (err) {
-            if (err) {
-                throw err;
-            }
-        })
+async function upload_photo(image, filename) {
+    console.log(`[${filename}] uploading in server storage...`);
 
-    });
+    if (!fs.existsSync('tmp')){
+        fs.mkdirSync('tmp');
+    }
+    const path = `tmp/${filename}`;
+    await image.mv(path);
+
+    console.log(`[${filename}] uploading to minio...`);
+
+    await minio_put(filename, path);
 }
 
-
-
-app.listen(3000, () => {
-    console.log("App listening on port 3000")
-})
 app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.post('/post_incoming', async function (req, res) {
-    let filename = req.body["filename"]
-    let path = __dirname + '/tmp/' + filename
+    const image = req.files.image;
+    if (!image) return;
+
+    let filename = req.files.image.name;
+
+    console.log(`POST incoming car plate ${filename}`);
+
+    await upload_photo(image, filename)
+
     let data = {
         "filename": filename,
         "incoming": true
     };
-    const image = req.files.image;
-    if (!image) return;
-
-    await upload_photo(image, filename, path)
 
     rabbit_sender(data)
 
-    res.end(JSON.stringify(req.body.filename))
+    res.end('Ok. Incoming plate image posted.')
 })
 
-
 app.post('/post_outcoming', async function (req, res) {
-    let filename = req.body["filename"]
-    let path = __dirname + '/tmp/' + filename
-    console.log(path)
+    const image = req.files.image;
+    if (!image) return;
+
+    let filename = req.files.image.name;
+
+    console.log(`POST outcoming car plate ${filename}`);
+
+    await upload_photo(image, filename)
+
     let data = {
         "filename": filename,
         "incoming": false
     };
-    const image = req.files.image;
-    if (!image) return;
-
-    await upload_photo(image, filename, path)
 
     rabbit_sender(data)
 
-    res.end(JSON.stringify(req.body.filename))
+    res.end('Ok. Outcoming plate image posted.')
+})
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000")
 })
